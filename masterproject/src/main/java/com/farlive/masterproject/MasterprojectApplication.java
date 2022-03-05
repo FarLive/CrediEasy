fe54fb0252e27a38b311ad3254eed5ca4e4cad0f
Rodrigo Ortiz <farliveg@gmail.com>
2022-03-04 19:24:22 -0600

    init

---
+++ masterproject/src/main/java/com/farlive/masterproject/MasterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.farlive.masterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterprojectApplication.class, args);
+	}
+
+}
---
+++ masterproject/src/test/java/com/farlive/masterproject/MasterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farlive.masterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
